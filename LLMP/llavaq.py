4a5362434d2259577406175007491b1bc25fd1b9
Daniel Haehn <haehn@mpsych.org>
2024-02-03 16:59:10 -0500

    First comparison between LLaVA and ChatGPT.

--- LLMP/llavaq.py
+++ LLMP/llavaq.py
@@ -1,13 +1,195 @@
 import os, sys
 sys.path.append(os.path.abspath('LLaVA/'))
 
+import argparse
 import png
 import io
 import numpy as np
 import base64
 
 
+import torch
+
+from llava.constants import (
+    IMAGE_TOKEN_INDEX,
+    DEFAULT_IMAGE_TOKEN,
+    DEFAULT_IM_START_TOKEN,
+    DEFAULT_IM_END_TOKEN,
+    IMAGE_PLACEHOLDER,
+)
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.mm_utils import (
+    process_images,
+    tokenizer_image_token,
+    get_model_name_from_path,
+    KeywordsStoppingCriteria,
+)
+
+from PIL import Image
+import requests
+
+
+
+
 class LLaVA:
+
+
+    @staticmethod
+    def query2(question, image, model_path="liuhaotian/llava-v1.5-7b"):
+
+        disable_torch_init()
+
+        tokenizer, model, image_processor, context_len = load_pretrained_model(
+            model_path=model_path,
+            model_base=None,
+            model_name=get_model_name_from_path(model_path)
+        )
+
+
+        args = type('Args', (), {
+            "model_path": model_path,
+            "model_base": None,
+            "model_name": get_model_name_from_path(model_path),
+            "query": question,
+            "conv_mode": None,
+            "image_file": None,
+            "sep": ",",
+            "temperature": 0,
+            "top_p": None,
+            "num_beams": 1,
+            "max_new_tokens": 1024
+        })()
+
+
+
+        qs = args.query
+        image_token_se = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
+        if IMAGE_PLACEHOLDER in qs:
+            if model.config.mm_use_im_start_end:
+                qs = re.sub(IMAGE_PLACEHOLDER, image_token_se, qs)
+            else:
+                qs = re.sub(IMAGE_PLACEHOLDER, DEFAULT_IMAGE_TOKEN, qs)
+        else:
+            if model.config.mm_use_im_start_end:
+                qs = image_token_se + "\n" + qs
+            else:
+                qs = DEFAULT_IMAGE_TOKEN + "\n" + qs
+
+        model_name = args.model_name
+
+
+        if "llama-2" in model_name.lower():
+            conv_mode = "llava_llama_2"
+        elif "v1" in model_name.lower():
+            conv_mode = "llava_v1"
+        elif "mpt" in model_name.lower():
+            conv_mode = "mpt"
+        else:
+            conv_mode = "llava_v0"
+
+        if args.conv_mode is not None and conv_mode != args.conv_mode:
+            print(
+                "[WARNING] the auto inferred conversation mode is {}, while `--conv-mode` is {}, using {}".format(
+                    conv_mode, args.conv_mode, args.conv_mode
+                )
+            )
+        else:
+            args.conv_mode = conv_mode
+
+        conv = conv_templates[args.conv_mode].copy()
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+
+        # print(prompt)
+
+        # image_files = image_parser(args)
+        # images = load_images(image_files)
+
+        size = image.shape[0]
+        grayscale = np.zeros((size,size), dtype=np.uint8)
+        grayscale[image==0] = 255
+        grayscale[image==1] = 0
+
+        images = []
+        pil_image = Image.fromarray(grayscale).convert("RGB")
+        images.append( pil_image )
+
+
+        # a = "https://llava-vl.github.io/static/images/view.jpg"
+
+        # def load_image(image_file):
+        #     if image_file.startswith("http") or image_file.startswith("https"):
+        #         response = requests.get(image_file)
+        #         image = Image.open(io.BytesIO(response.content)).convert("RGB")
+        #     else:
+        #         image = Image.open(image_file).convert("RGB")
+        #     return image
+
+
+        # def load_images(image_files):
+        #     out = []
+        #     for image_file in image_files:
+        #         image = load_image(image_file)
+        #         out.append(image)
+        #     return out
+
+
+        # image_files = [a]#image_parser(a)
+        # images = load_images(image_files)
+
+        images_tensor = process_images(
+            images,
+            image_processor,
+            model.config
+        ).to(model.device, dtype=torch.float16)
+
+        input_ids = (
+            tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt")
+            .unsqueeze(0)
+            .cuda()
+        )
+
+        stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+        keywords = [stop_str]
+        stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+
+        with torch.inference_mode():
+            output_ids = model.generate(
+                input_ids,
+                images=images_tensor,
+                do_sample=True if args.temperature > 0 else False,
+                temperature=args.temperature,
+                top_p=args.top_p,
+                num_beams=args.num_beams,
+                max_new_tokens=args.max_new_tokens,
+                use_cache=True,
+                stopping_criteria=[stopping_criteria],
+            )
+
+        input_token_len = input_ids.shape[1]
+        # n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+        # if n_diff_input_output > 0:
+        #     print(
+        #         f"[Warning] {n_diff_input_output} output_ids are not the same as the input_ids"
+        #     )
+        # outputs = tokenizer.batch_decode(
+        #     output_ids[:, input_token_len:], skip_special_tokens=True
+        # )[0]
+        outputs = tokenizer.batch_decode(
+            output_ids[:, :], skip_special_tokens=True
+        )[0]
+        outputs = outputs.strip()
+        if outputs.endswith(stop_str):
+            outputs = outputs[: -len(stop_str)]
+        outputs = outputs.strip()
+
+        return outputs
+        # print(outputs)
+
+
 
     @staticmethod
     def query(question, image, model_path="liuhaotian/llava-v1.5-7b"):
@@ -21,6 +203,10 @@ class LLaVA:
             model_base=None,
             model_name=get_model_name_from_path(model_path)
         )
+
+
+
+
 
 
         size = image.shape[0]
