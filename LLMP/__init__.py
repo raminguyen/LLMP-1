8d85658035fcaa613325933b1e28b3b82d8f3c29
Kenichi Maeda <kenichi.maeda001@chimera.umb.edu>
2024-09-30 14:24:44 -0400

    Remove customLLaVA from init

--- LLMP/__init__.py
+++ LLMP/__init__.py
@@ -4,7 +4,6 @@ from .llavaq import LLaVAModel
 from .evaluator import Evaluator
 from .evaluator2 import Evaluator2
 from .evaluator4 import Evaluator4
-from .customllava import CustomLLaVA
 from dotenv import load_dotenv
 import os
 from .gemini import GeminiBaseModel, GeminiProVision, Gemini1_5Flash
